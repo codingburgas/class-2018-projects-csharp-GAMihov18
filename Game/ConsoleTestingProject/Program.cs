78442b5fbc4045ddc46f10cbf1744cf771bab333
Georgi Mihov <GAMihov18@codingburgas.bg>
2022-05-08 16:38:49 +0300

    Add helper methods to Generator class

--- Game/ConsoleTestingProject/Program.cs
+++ Game/ConsoleTestingProject/Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-
+using GeneratorLib.Generator;
 namespace ConsoleTestingProject
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
-
+			Generator generator = new Generator();
+			generator.
 		}
 	}
 }
--- Game/Generators/Generator/Generator.cs
+++ Game/Generators/Generator/Generator.cs
@@ -6,13 +6,21 @@ namespace GeneratorLib.Generator
 	public class Generator
 	{
 		Queue<Weapon> weaponQueue;
-		//Add queues for Armor and Player when added
+		Queue<Armor> armorQueue;
+		//Add queues for Player when added
 
 		//Unimplemented point system the generator will use
 		int pointsAllowed = 0;
+		public Generator()
+		{
+			weaponQueue = new Queue<Weapon>();
+			armorQueue = new Queue<Armor>();
+			pointsAllowed = 100;
+		}
 		public Generator(int pointsAllowed)
 		{
 			weaponQueue = new Queue<Weapon>();
+			armorQueue = new Queue<Armor>();
 			this.pointsAllowed = pointsAllowed;
 		}
 		public bool GenerateWeapons(int numberOfWeapons)
@@ -23,16 +31,44 @@ namespace GeneratorLib.Generator
 			{
 				weaponQueue.Enqueue(new Weapon());
 			}
-			
+
+			return true;
+		}
+
+		public bool GenerateArmors(int numberOfArmors)
+		{
+
+			armorQueue = new Queue<Armor>();
+			for (int i = 0; i < numberOfArmors; i++)
+			{
+				armorQueue.Enqueue(new Armor());
+			}
 			return true;
 		}
 		public Weapon NextWeapon()
 		{
 			if (weaponQueue.Count == 0)
 			{
-				GenerateWeapons(1);
+				return null;
 			}
 			return weaponQueue.Dequeue();
+		}
+		public Armor NextArmor()
+		{
+			if (armorQueue.Count == 0)
+			{
+				return null;
+			}
+			return armorQueue.Dequeue();
+		}
+		public void ClearMemory()
+		{
+			weaponQueue.Clear();
+			armorQueue.Clear();
+		}
+		public void DumpDebug()
+		{
+
 		}
 	}
 }
